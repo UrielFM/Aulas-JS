b36e7f485a1f77b1ff616a318bf89d01c845018d
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-05 15:11:50 -0300

    12

---
+++ js/01-sintaxe.js
@@ -0,0 +1,34 @@
+console.log("Hello World");
+console.log('Olá Mundo');
+
+// Comentário - CRTL + ;
+/* Comentário - ALT + SHIFT + A */
+
+/* Manipulando dados */
+
+// Variavéis
+
+let curso = "HTML5 e CSS3";
+let ano = 2023
+
+
+
+// Constantes
+const mediaMinima = 7;
+
+// Sáda de dados a partir da memoria
+console.log (curso);
+console.log (mediaMinima);
+console.log (ano);
+
+
+// Sa~ida de dados personalizada
+
+/* Estou fazendo o curso de HTML5 e CSS3 neste ano de 2023 */
+
+// Templete String/Literal: saida usando crase e ${}
+console.log (`Estou fazendo o curso de ${curso} neste ano de ${ano}.`)
+
+// Concatenação (forma mais antigas de saída, mas ainda válida)
+console.log("Estou fazendo o curso de "+curso+" neste ano de " +ano+ ".");
+
