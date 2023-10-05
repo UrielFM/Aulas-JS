907fe340355a691e3870460228e22192c930b556
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-05 16:49:59 -0300

    exercicio arrays

--- js/02-estrutura-de-dados.js
+++ js/02-estrutura-de-dados.js
@@ -8,9 +8,13 @@ Objetos
 Lista de dados não indexado e formados por propriedads e valores.
 */
 
-// Arrays
+// Arrays (os indices sempre começam no zero)
 
 let cursos = ["Node.js", "React", "SQL", "UX/UI Design"];
 
 console.log(cursos[2]); //SQL
-console.log(cursos); // saída direta/estrutura
+console.log(cursos); // saída direta/estrutura (APENAS NO CONSOLE)
+
+
+const dados = ['Uriel', 'Mishima', 46, "99999-9999", "São Paulo"]
+console.log(`${dados[0]} tem ${dados [2]} anos.`)
