075dd76186ca15852d8ebe7d98f08555bc3def56
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-05 17:12:31 -0300

    Objetos

--- js/02-estrutura-de-dados.js
+++ js/02-estrutura-de-dados.js
@@ -18,3 +18,18 @@ console.log(cursos); // saída direta/estrutura (APENAS NO CONSOLE)
 
 const dados = ['Uriel', 'Mishima', 46, "99999-9999", "São Paulo"]
 console.log(`${dados[0]} tem ${dados [2]} anos.`)
+
+// Objeto
+
+let carro = {
+    marca: "Fiat",
+    modelo: "Uno com escada",
+    portas: 2,
+    cor: "branco",
+    opcionais: ["Ar-Condionado", "Vidros Eletricos", "Alarmes"]
+};
+console.log(carro);
+console.log(carro.modelo);
+
+console.log(
+    `Carros da ${carro.modelo} tem a cor ${carro.cor} e opcionais ${carro.opcionais[2]}.`);
