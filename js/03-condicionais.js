3923ea85093ecaa880c1d90155d95ce364aff6e7
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-10 15:27:12 -0300

    prompt

--- js/03-condicionais.js
+++ js/03-condicionais.js
@@ -6,8 +6,8 @@ o programa pode executar ações diferentes.
 Comandos mais comuns: if(se), else(senão), else if (senão se)
 */
 
-let usuario = "Juean";
-let idade = 20;
+let usuario = prompt("Qual é seu nome?");
+let idade = prompt("Quantos anos vocè tem?");
 let mensagem; // indefinida/underfined
 
 // Verificar a idade e determinar se é menos ou maior de idade
