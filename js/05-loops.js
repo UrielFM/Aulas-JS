0789f240f45c33bde95b12f5be473c6d43d79499
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-11 16:20:00 -0300

    do while

--- js/05-loops.js
+++ js/05-loops.js
@@ -11,10 +11,17 @@ Comandos Tradicionais
 Obs.: Normalamente o loop é controlado através de uma variável contadora.
 */
 
-let contador = 1;
-while (contador <=5) {
-    console.log(`Valor de contador: ${contador}`);
-    contador++; // ++ incremento
-}
+// let contador = 1;
+// while (contador <=5) {
+//     console.log(`Valor de contador: ${contador}`);
+//     contador++; // ++ incremento
+// }
 
 
+// EXEMPLO DO/WHILE (FAÇA/ENQUANTO)
+
+let contador = 1
+do {
+    console.log(`Contador vale: ${contador}`);
+    contador++;
+} while (contador <= 3);
