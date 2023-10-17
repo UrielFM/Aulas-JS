bde6ed0c04501c3c4ddd37ac77ae35e6da97e220
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-17 13:35:47 -0300

    16

--- js/exercicios02.js
+++ js/exercicios02.js
@@ -1,25 +1,21 @@
-let alunos [ 
+let alunos = [ 
     {
         nome:"Joãozinho",
         Id:15,
         Escola: "Gabriel",
         ano: "2016",
-    };
+    },
     {
         Nome: "Mariazinha",
         Id: 18,
         Escola: "Almirante",
         Ano: "2018",
-    };
+    },
     {
         Nome: "Giuseppezinho",
         Id: 12,
         Escola: "Senne",
         Ano: "2022",
-    };
+    },
 ];
 
-let nomes = alunos.length;
-
-for(let i = 0; i < quantidade; i++)
-    console.log(`Clientes: ${clientes[i]}`);
