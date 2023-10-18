1f804dbafb7164e08e655e1c5894267d06c27b3b
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-18 16:53:06 -0300

    Selecionando os links contidos na lista

--- js/07-dom-eventos.js
+++ js/07-dom-eventos.js
@@ -32,3 +32,21 @@ const variosElementos = document.querySelectorAll("h3, a");
 
 console.log(variosElementos);
 console.log(variosElementos[1]);
+
+
+// Modificando o DOM
+
+const titulo = document.querySelector("h1");
+titulo.textContent = "Olá JavaScript!";
+titulo.style.textAlign = "center"; //CSS inline via JS
+titulo.classList.add("destaque"); //Classe CSS via JS
+
+/* Selecionando os links contidos na lista */
+const linksReferencia = document.querySelectorAll("ul li a");
+
+let quantidadeDeLinks = linksReferencia.length
+
+for (let i = 0; i < quantidadeDeLinks; i++) {
+     linksReferencia[i].setAttribute("target","_blank")
+}
+console.log(linksReferencia);
