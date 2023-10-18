474acd8ac173104a332ee3bd2f4b16f75859be32
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-18 16:13:21 -0300

    getElementById()

---
+++ js/07-dom-eventos.js
@@ -0,0 +1,20 @@
+"use strict"
+
+/* Funções de seleção no DOM
+
+-getElementById()
+Seleciona UM elemento atraves de um ID.
+
+-querySelector()
+Seleciona UM elemento atravès de um seletor.
+
+-querySetectorAll()
+Seleciona VÁRIOS elemento atravès de um/vários seletor(es).
+*/
+
+/* Exemplos */
+
+const subtitulo = document.getElementById("subtitulo-dom");
+
+console.log(subtitulo);
+
