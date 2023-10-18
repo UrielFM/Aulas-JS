71f4b4df4dc376a92421ce8aa30c68d56b2576d7
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-18 14:30:35 -0300

    Comentarios

--- js/exercicios03.js
+++ js/exercicios03.js
@@ -1,3 +1,6 @@
+"use strict"
+
+// Função que realiza a conversão matématica ao receber PARÂMENTROS.
 function converterMoeda(valor, taxaDeCambio) {
     return valor * taxaDeCambio;
 }
@@ -6,11 +9,15 @@ function formatarValor(valor, locale, currency) {
     return valor.toLocaleString(locale, { style: 'currency', currency: currency });
 }
 
-let valorEmDolar = 1000;
+// Dados de entrada(exemplos de valores).
+let valorEmDolar = 2500;
 let taxaDeCambioParaReal = 5.03;
 
+/* Variável que receberá o rsultado processado pela função DEPOIS que a função terminar sua execução. */
 let valorEmReal = converterMoeda(valorEmDolar, taxaDeCambioParaReal);
 
+
+// Saidas após o processamento.
 console.log(`Valor em Dólar:`, formatarValor(valorEmDolar, 'en-US', 'USD'));
 console.log(`Valor em Real:`, formatarValor(valorEmReal, 'pt-BR', 'BRL'));
 console.log(`Taxa de Câmbio:`, formatarValor(taxaDeCambioParaReal, 'pt-BR', 'BRL'));
