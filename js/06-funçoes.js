884112ebe231e7287c4e525bce59aa2e2d324f86
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-18 15:08:29 -0300

    Arrow Function versão 1

--- js/06-funçoes.js
+++ js/06-funçoes.js
@@ -77,3 +77,27 @@ console.log(`Desconto: ${formatarPreco(desconto)}`);
 
 console.log(`Preço final: ${formatarPreco(precoFinal)}`);
 
+
+console.log("--------------");
+
+/* Sobre Arrow Function
+Sintaxe moderna para funções no JS, bastante usada por bibliotecas, frameworks(React, Angular, Vue e etc).
+
+A sintaxe geral é:
+
+const algumNome = () => {}
+
+No entanto, esta sintaxe por ser mais concisa.
+*/
+
+// Versão 1
+function dobra(valor) {
+    return valor * 2
+}
+
+
+
+// Chamada
+console.log(dobra(10));
+console.log(dobra (1325));
+console.log(dobra(55.47));
