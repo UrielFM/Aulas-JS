45a04c0005c5c90c1cc66178b6c6347c28c361e3
URIEL FARIAS MISHIMA <uriel.mishima77@gmail.com>
2023-10-18 16:20:15 -0300

    exemplos querySetector()

--- js/07-dom-eventos.js
+++ js/07-dom-eventos.js
@@ -14,7 +14,17 @@ Seleciona VÁRIOS elemento atravès de um/vários seletor(es).
 
 /* Exemplos */
 
+// getElementById()
 const subtitulo = document.getElementById("subtitulo-dom");
-
 console.log(subtitulo);
 
+
+// querySelector()
+const desenho = document.querySelector("figure");
+const descricao =  document.querySelector("#descricao-dom");
+
+console.log(desenho);
+console.log(descricao);
+
+
+// 
